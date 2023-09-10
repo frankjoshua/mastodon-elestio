22a2d4f7660e61cbdade7105e948867f4f24b0bd
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-10 17:16:15 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -3,3 +3,20 @@ set -o allexport; source .env; set +o allexport;
 
 mkdir -p ./storage/opensearch
 chown -R 1000:1000 ./storage/opensearch
+
+cat <<EOT > ./servers.json
+{
+    "Servers": {
+        "1": {
+            "Name": "local",
+            "Group": "Servers",
+            "Host": "172.17.0.1",
+            "Port": 6754,
+            "MaintenanceDB": "postgres",
+            "SSLMode": "prefer",
+            "Username": "postgres",
+            "PassFile": "/pgpass"
+        }
+    }
+}
+EOT
